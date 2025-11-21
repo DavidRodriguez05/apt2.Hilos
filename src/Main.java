05ef62aad3471e1535964c0a5577b2b30f4e06c1
FP <davidrsm051205@gmail.com>
2025-11-21 13:49:31 +0100

    creacion de Main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Completar Codigo
+        Barco barco = new Barco();
 
+        Rescate rescate = new Rescate(barco.getPasajeros());
+        Thread rescateHilo = new Thread(rescate);
+        rescateHilo.start();
+
+        try {
+            rescateHilo.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Main: Programa terminado.");
     }
 }
