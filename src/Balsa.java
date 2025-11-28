b79700bd786e7cd3d0d75ab209e51c61be4810b7
FP <davidrsm051205@gmail.com>
2025-11-28 13:51:27 +0100

    Implementacion de Balsa

--- src/Balsa.java
+++ src/Balsa.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Balsa extends Thread {
 
@@ -7,12 +8,14 @@ public class Balsa extends Thread {
     private final int capacidad;
     private final long tiempoRescate;
     private final List<Pasajero> arrayCompartido;
+    private final Semaphore semaforo;
 
-    public Balsa(String nombre, int capacidad, long tiempoRescateMs, List<Pasajero> arrayCompartido) {
+    public Balsa(String nombre, int capacidad, long tiempoRescateMs, List<Pasajero> arrayCompartido, Semaphore semaforo) {
         this.nombre = nombre;
         this.capacidad = capacidad;
         this.tiempoRescate = tiempoRescateMs;
         this.arrayCompartido = arrayCompartido;
+        this.semaforo = semaforo;
     }
 
     @Override
@@ -21,11 +24,20 @@ public class Balsa extends Thread {
         while (true) {
             List<Pasajero> rescatadosArray = new ArrayList<>();
 
+            try {
+                // Adquirir el semáforo para acceder al recurso compartido
+                semaforo.acquire();
 
-            synchronized (arrayCompartido) {
                 for (int i = 0; i < capacidad && !arrayCompartido.isEmpty(); i++) {
                     rescatadosArray.add(arrayCompartido.remove(0));
                 }
+
+                // Liberar el semáforo
+                semaforo.release();
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
             }
 
 
